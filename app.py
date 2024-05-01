489111b8977086d57c16343ef732f292b52f7cb1
thigosantos0 <thigosantosgg0@gmail.com>
2024-05-01 15:20:54 -0300

    Adding a mock program file

---
+++ app.py
@@ -0,0 +1,24 @@
+
+
+
+
+#IFFT.If(foo1 block)
+def foo1(number1: int, number2: int) -> int:
+    # Adding a change for testing purposes
+    return number1 + number2
+
+# Adding a change for testing purposes
+
+#IFFT.Then("./app2.py", "../foo1_related_block")
+
+def foo2(number1: int, number2: int) -> int:
+    return number1 - number2
+
+
+
+
+# main
+
+print(foo1(1, 2))
+print(foo2(1, 2))
+

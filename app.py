d1b450be0d4f5c8bbec4cbe7fe89992b93e147be
Thiago Santos <thiagosantosgg0@gmail.com>
2024-05-15 15:16:26 -0300

    Adding more lines for manual tests

--- app.py
+++ app.py
@@ -10,15 +10,31 @@ def foo1(number1: int, number2: int) -> int:
 
 # Adding a change for testing purposes
 
+def foo5(number1: int, number2: int) -> int:
+    return number1 ** number2
+
+def foo6(number1: int, number2: int) -> int:
+    return number1 % number2
+
+def foo7(number1: int, number2: int) -> int:
+    return number1 // number2
+
 #IFFT.Then("file1.py", "foo1_related_block")
 
 def foo2(number1: int, number2: int) -> int:
     return number1 - number2
 
+def foo3(number1: int, number2: int) -> int:
+    return number1 * number2
 
+def foo4(number1: int, number2: int) -> int:
+    return number1 / number2
 
+def foo8(number1: int, number2: int) -> int:
+    return 2*number1 + number2
 
 # main
 
 print(foo1(1, 2))
 print(foo2(1, 2))
+print(foo3(1, 2))

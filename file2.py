ac4c0deb4ebde6ef41690a0770c0333d2f6bbdae
thigosantos0 <thigosantosgg0@gmail.com>
2025-01-05 10:38:24 -0300

    Adding new files for testing

---
+++ file2.py
@@ -0,0 +1,10 @@
+#IFFT.If(foo3_related_block)
+
+print('Hello world!')
+print('Hello world 2!')
+print('Hello world, the return!')
+print('Hello world, the return!')
+
+print("Modifying file2")
+
+#IFFT.Then("app.py", "foo1_block")
---
+++ file3.py
@@ -0,0 +1,12 @@
+#IFFT.If(foo4_related_block)
+
+print('Hello world!')
+print('Hello world 2!')
+print('Hello world, the return!')
+print('Hello world, the return!')
+
+print("More content")
+print("Adding more content")
+
+
+#IFFT.Then("app.py", "foo1_block")
